479845280c7aa59fc00bce2a0cd1c8957b5b609f
Shahariyr reza <99033913+ShahariyrReza@users.noreply.github.com>
2022-09-16 11:56:41 +0600

    Update binary search.c

--- binary search.c
+++ binary search.c
@@ -4,17 +4,17 @@ int binarySearch(int a[], int beg, int end, int val)
     int mid;
     if(end >= beg)
     {        mid = (beg + end)/2;
-/* if the item to be searched is present at middle */
+// if the item to be searched is present at middle
         if(a[mid] == val)
         {
             return mid+1;
         }
-            /* if the item to be searched is smaller than middle, then it can only be in left subarray */
+            //if the item to be searched is smaller than middle, then it can only be in left subarray 
         else if(a[mid] < val)
         {
             return binarySearch(a, mid+1, end, val);
         }
-            /* if the item to be searched is greater than middle, then it can only be in right subarray */
+            // if the item to be searched is greater than middle, then it can only be in right subarray
         else
         {
             return binarySearch(a, beg, mid-1, val);
@@ -25,8 +25,8 @@ int binarySearch(int a[], int beg, int end, int val)
 int main() {
   int a[] = {11, 14, 25, 30, 40, 41, 52, 57, 70}; // given array
   int val = 40; // value to be searched
-  int n = sizeof(a) / sizeof(a[0]); // size of array
-  int res = binarySearch(a, 0, n-1, val); // Store result
+  int n = sizeof(a) / sizeof(a[0]); // size of the array
+  int res = binarySearch(a, 0, n-1, val); // Stored result
   printf("The elements of the array are - ");
   for (int i = 0; i < n; i++)
   printf("%d ", a[i]);
